06764e8157068bf1ba72ecc30dadf6ae2e609225
developer-rni <developer.rni@gmail.com>
2021-02-09 00:35:54 +0300

    added work of date and time in the main window

--- ShutdownPC.py
+++ ShutdownPC.py
@@ -12,6 +12,13 @@ class Ui_ShutdownPC(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.dragPos = QtCore.QPoint()
 
+        self.timeEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.dateEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.dateEdit_2.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+        self.timer.start(1000)
+
         self.pushButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
         self.pushButton_3.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
         self.btn_close.clicked.connect(self.close)
@@ -20,7 +27,13 @@ class Ui_ShutdownPC(QtWidgets.QMainWindow, Ui_MainWindow):
         QtCore.QMetaObject.connectSlotsByName(self)
 
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        # self.setWindowFlags(QtCore.Qt.CustomizeWindowHint)    # изменение размера окна
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+    def showTime(self):
+        self.timeEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.dateEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.dateEdit_2.setDateTime(QtCore.QDateTime.currentDateTime())
 
     def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
